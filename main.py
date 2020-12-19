b6359667a0cf138d21a04d62a20abc15969310dd
K.DH3 <30466064+331leo@users.noreply.github.com>
2020-12-19 21:31:54 +0900

    feat: text color enchantment

--- main.py
+++ main.py
@@ -7,18 +7,43 @@ import shutil
 import psutil
 import math
 import platform
-versionnum="CLI_1.1.6"
+
+
+
+versionnum="CLI_1.2.0"
 _ = os.system("title Youtube Downloader (Made By Leok.kr)")
 run_time=str(datetime.datetime.now())[:19].replace(":","_")
 isWindows=False
 isMac=False
 isLinux=False
+isNvidia=False
+gpuNames=[]
 if platform.system() == 'Windows':
     isWindows = True
 elif platform.system() == "Darwin":
     isMac = True
 elif platform.system() == "Linux":
     isLinux = True
+
+if isWindows:
+    import wmi
+    gpus=wmi.WMI().Win32_VideoController()
+    for g in gpus:
+        if g.Name.lower().startswith("nvidia"):
+            isNvidia=True
+        gpuNames.append(g.Name)
+
+class tenchant:
+    pink = '\033[95m'
+    blue = '\033[94m'
+    mint = '\033[96m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    ori = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
 
 def clear():
     if isWindows:
@@ -66,6 +91,7 @@ def checkffmpeg():
     clear()
 
 def download(o_type,linkinputmode):
+    run_time = str(datetime.datetime.now())[:19].replace(":", "_")
     links = []
     if o_type == "mp3":
         o_type_human = "음악파일(mp3)"
@@ -101,7 +127,7 @@ def download(o_type,linkinputmode):
         if not os.path.isfile(f"download.txt"):
             f = open(f"download.txt", "w")
             f.close()
-            input(f"download.txt 파일이 존재하지 않습니다.\n해당 파일을 생성하였으니, 해당 파일에 {o_type_human}로 다운받고 싶은 유튜브 영상들의 링크를 넣으신후 다시 실행해 주세요.\n\n> 엔터를 누르시면 메뉴로 돌아갑니다. ")
+            input(f"download.txt 파일이 존재하지 않습니다.\n해당 파일을 생성하였으니, 해당 파일에 {o_type_human}로 다운받고 싶은 유튜브 영상들의 링크를 넣으신후 다시 실행해 주세요.\n\n{tenchant.bold}{tenchant.mint}> 엔터를 누르시면 메뉴로 돌아갑니다. {tenchant.ori}")
             return
         else:
             f=open(f"download.txt","r",encoding="UTF-8")
@@ -109,7 +135,8 @@ def download(o_type,linkinputmode):
             f.close()
     elif linkinputmode == "2":
         linput_type = "사용자 입력 링크"
-        links.append(input(f"> {o_type_human}로 다운받을 영상의 링크를 입력하세요: "))
+        links.append(input(f"{tenchant.yellow}> {o_type_human}로 다운받을 영상의 링크를 입력하세요: "))
+        print(f"{tenchant.ori}")
 
     if len(links) >= 1:
         if len(links[0]) >= 5:
@@ -121,25 +148,25 @@ def download(o_type,linkinputmode):
                     ydl.download(links)
             except Exception as e:
                 clear()
-                print(f"다운로드가 불가능 합니다.\n{e}\n")
-                input("> 엔터를 누르시면 메뉴로 돌아갑니다. ")
+                print(f"{tenchant.red}다운로드가 불가능 합니다.\n{e}\n{tenchant.ori}")
+                input(f"{tenchant.bold}{tenchant.mint}> 엔터를 누르시면 메뉴로 돌아갑니다. {tenchant.ori}")
                 return
             if linput_type == "download.txt":
                 shutil.move(f"./download.txt",f"./downloaded_{o_type}/{run_time}/!downloaded_{o_type}_links.txt")
                 f = open(f"download.txt", "w")
                 f.close()
             clear()
-            print(f"다운로드가 완료되었습니다.\n./downloaded_{o_type}/{run_time} 에 저장하였습니다!\n{linput_type} 를 초기화 하였습니다!")
+            print(f"{tenchant.green}다운로드가 완료되었습니다.{tenchant.ori}\n./downloaded_{o_type}/{run_time} 에 저장하였습니다!\n{linput_type} 를 초기화 하였습니다!")
             if isWindows:
                 os.system(f"explorer downloaded_{o_type}\{run_time}")
                 print("\n다운로드된 폴더를 열었습니다!")
-            input("> 엔터를 누르시면 메뉴로 돌아갑니다. ")
+            input(f"{tenchant.bold}{tenchant.mint}> 엔터를 누르시면 메뉴로 돌아갑니다. {tenchant.ori}")
             return
         else:
-            input(f"{linput_type} 이(가) 비어있거나, 올바르지 않습니다.\n해당 파일에 {o_type_human}로 다운받고 싶은 유튜브 영상들의 링크를 넣어주세요.\n\n> 엔터를 누르시면 메뉴로 돌아갑니다. ")
+            input(f"{linput_type} 이(가) 비어있거나, 올바르지 않습니다.\n해당 파일에 {o_type_human}로 다운받고 싶은 유튜브 영상들의 링크를 넣어주세요.\n\n{tenchant.bold}{tenchant.mint}> 엔터를 누르시면 메뉴로 돌아갑니다. {tenchant.ori}")
             return
     else:
-        input(f"{linput_type} 이(가) 비어있거나, 올바르지 않습니다.\n해당 파일에 {o_type_human}로 다운받고 싶은 유튜브 영상들의 링크를 넣어주세요.\n\n> 엔터를 누르시면 메뉴로 돌아갑니다. ")
+        input(f"{linput_type} 이(가) 비어있거나, 올바르지 않습니다.\n해당 파일에 {o_type_human}로 다운받고 싶은 유튜브 영상들의 링크를 넣어주세요.\n\n{tenchant.bold}{tenchant.mint}> 엔터를 누르시면 메뉴로 돌아갑니다. {tenchant.ori}")
         return
 
 
@@ -151,7 +178,8 @@ def menu():
             print("[1] 파일로 부터 링크 가져오기(많은 영상 동시 다운로드)")
             print("[2] 영상 링크 직접 입력(하나씩 다운로드)")
             print("[q] 메인메뉴")
-            c = input("> ")
+            c = input(f"{tenchant.bold}{tenchant.mint}> ")
+            print(f"{tenchant.ori}")
             clear()
             if c == "1" or c == "2" or c == "q":
                 ok = False
@@ -163,7 +191,8 @@ def menu():
     print("[2] 영상 다운로드 모드(포멧: mkv)")
     print("[3] 영상 다운로드 모드(강제 mp4변환, 오래걸림)")
     print("[q] 종료")
-    c = input("> ")
+    c = input(f"{tenchant.bold}{tenchant.mint}> ")
+    print(f"{tenchant.ori}")
     clear()
 
     if c == "1":
@@ -178,15 +207,17 @@ def menu():
             return 1
         download("ori_video",r)
     elif c == "3":
+        if not isNvidia:
+            print(f"{tenchant.yellow}이 컴퓨터로는 mp4변환이 매우 느립니다.\n꼭 mp4가 필요한것이 아니라면, 메인으로 돌아가 2번 원본영상 다운로드를 이용해주세요.\n{tenchant.ori}")
         r = isfilemode()
         if r == "q":
             return 1
         download("mp4",r)
     elif c == "i":
         clear()
-        infotext = f"**INFO**\n\nMade by: LeoK (leok.kr)\nSpecial Thanks to: Youtube_Dl, FFMPEG\nPlatform: {platform.platform()}, {psutil.Process(os.getppid()).name()}\nVersion: {versionnum}\nSupport: support@leok.kr"
+        infotext = f"**INFO**\n\nMade by: LeoK (leok.kr)\nSpecial Thanks to: Youtube_Dl, FFMPEG\nPlatform: {platform.platform()}, {psutil.Process(os.getppid()).name()}\nGPU name: {gpuNames}\nHwaccel: {isNvidia}\nVersion: {versionnum}\nSupport: support@leok.kr"
         print(infotext)
-        input("\n> 엔터를 누르면 메인화면으로 돌아갑니다.")
+        input(f"\n{tenchant.bold}{tenchant.mint}> 엔터를 누르면 메인화면으로 돌아갑니다.{tenchant.ori}")
         clear()
     elif c == "q":
         return 0
